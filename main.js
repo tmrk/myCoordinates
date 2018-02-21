8a2f71d16a4f2febf93207773cf299af190a87ef
tmrk <tamas.marki@gmail.com>
2018-02-21 16:11:56 +1400

    10

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function getCoordinates() {
                      + 'Longitude: &#9;' + precisionRound(longitude, 10) + '<br/>'
                      + 'Altitude: &#9;' + precisionRound(altitude, 10) + '<br/>'
                      + 'Accuracy: &#9;' + precisionRound(accuracy, 10) + '</pre>'
-                     + '<a href="mailto:?Subject=My%20coordinates&body=' + encodeURIComponent(coords) + '">Send this in email</a>';
+                     + '<a href="mailto:tamas.marki@undp.org?Subject=My%20coordinates&body=' + encodeURIComponent(coords) + '">Send this in email</a>';
 
     var img = new Image();
     img.src = 'https://maps.googleapis.com/maps/api/staticmap?size=335x300&zoom=15&style=saturation:-100&style=feature:water|element:geometry.fill|lightness:100&center=' + latitude + ',' + longitude + '&markers=color:red%7C' + latitude + ',' + longitude + '&sensor=false&key=' + googleApiKey;
