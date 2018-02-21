249e0f49ab1973aa440268d30aad4761c91ac699
tmrk <tamas.marki@gmail.com>
2018-02-21 15:33:34 +1400

    9

--- main.js
+++ main.js
@@ -21,20 +21,20 @@ function getCoordinates() {
         altitude = position.coords.altitude,
         accuracy = position.coords.accuracy;
 
-    var coords = 'Latitude: ' + latitude + '\n'
-               + 'Longitude: ' + longitude + '\n'
-               + 'Altitude: ' + altitude + '\n'
-               + 'Accuracy: ' + accuracy;
+    var coords = 'Latitude: ' + precisionRound(latitude, 10) + '\n'
+               + 'Longitude: ' + precisionRound(longitude, 10) + '\n'
+               + 'Altitude: ' + precisionRound(altitude, 10) + '\n'
+               + 'Accuracy: ' + precisionRound(accuracy, 10);
 
     output.innerHTML = ''
-                     + '<pre>My current coorindates<br/><br/>Latitude: &#9;' + precisionRound(latitude, 10) + '<br/>'
+                     + '<pre>My current coordinates:<br/><br/>Latitude: &#9;' + precisionRound(latitude, 10) + '<br/>'
                      + 'Longitude: &#9;' + precisionRound(longitude, 10) + '<br/>'
                      + 'Altitude: &#9;' + precisionRound(altitude, 10) + '<br/>'
-                     + 'Accuracy: &#9;' + accuracy + '</pre>'
+                     + 'Accuracy: &#9;' + precisionRound(accuracy, 10) + '</pre>'
                      + '<a href="mailto:?Subject=My%20coordinates&body=' + encodeURIComponent(coords) + '">Send this in email</a>';
 
     var img = new Image();
-    img.src = 'https://maps.googleapis.com/maps/api/staticmap?size=335x300&zoom=15&style=feature:water|element:geometry.fill|lightness:100&center=' + latitude + ',' + longitude + '&markers=color:red%7C' + latitude + ',' + longitude + '&sensor=false&key=' + googleApiKey;
+    img.src = 'https://maps.googleapis.com/maps/api/staticmap?size=335x300&zoom=15&style=saturation:-100&style=feature:water|element:geometry.fill|lightness:100&center=' + latitude + ',' + longitude + '&markers=color:red%7C' + latitude + ',' + longitude + '&sensor=false&key=' + googleApiKey;
     output.appendChild(img);
   }
 
